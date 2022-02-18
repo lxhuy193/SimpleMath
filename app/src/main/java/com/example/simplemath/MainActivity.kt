20b8ee8eab9ae0f36a1bbe5fd7504a7a760bb9cf
kubomnhata <55189177+kubomnhata@users.noreply.github.com>
2022-02-18 16:32:14 +0700

    final

--- app/src/main/java/com/example/simplemath/MainActivity.kt
+++ app/src/main/java/com/example/simplemath/MainActivity.kt
@@ -7,52 +7,70 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 //        var btn_temp : Button = findViewById(R.id.btn_addition)
+        var num1 : EditText = findViewById<EditText>(R.id.et_input1)
+        var num2 : EditText = findViewById<EditText>(R.id.et_input2)
+        var result : TextView = findViewById<TextView>(R.id.tv_result)
 
-        when (findViewById<Button>(R.id.btn_addition).text.toString()){
-            "+" -> {
-                val btn_cong : Button = findViewById(R.id.btn_addition)
-                btn_cong.setOnClickListener {
-                    var num1 : Double = (findViewById<EditText>(R.id.et_input1).text.toString()).toDouble()
-                    var num2 : Double = (findViewById<EditText>(R.id.et_input2).text.toString()).toDouble()
-                    var result = num1 + num2
-                    findViewById<TextView>(R.id.tv_result).text = result.toString()
-                }
-            }
-
-            "-" -> {
-                val btn_tru : Button = findViewById(R.id.btn_subtraction)
-                btn_tru.setOnClickListener {
-                    var num1 : Double = (findViewById<EditText>(R.id.et_input1).text.toString()).toDouble()
-                    var num2 : Double = (findViewById<EditText>(R.id.et_input2).text.toString()).toDouble()
-                    var result = num1 - num2
-                    findViewById<TextView>(R.id.tv_result).text = result.toString()
-                }
-            }
-
-            "*" -> {
-                val btn_nhan : Button = findViewById(R.id.btn_multiply)
-                btn_nhan.setOnClickListener {
-                    var num1 : Double = (findViewById<EditText>(R.id.et_input1).text.toString()).toDouble()
-                    var num2 : Double = (findViewById<EditText>(R.id.et_input2).text.toString()).toDouble()
-                    var result = num1 * num2
-                    findViewById<TextView>(R.id.tv_result).text = result.toString()
-                }
-            }
-
-            "/" -> {
-                val btn_chia : Button = findViewById(R.id.btn_division)
-                btn_chia.setOnClickListener {
-                    var num1 : Double = (findViewById<EditText>(R.id.et_input1).text.toString()).toDouble()
-                    var num2 : Double = (findViewById<EditText>(R.id.et_input2).text.toString()).toDouble()
-                    var result = num1/num2
-                    findViewById<TextView>(R.id.tv_result).text = result.toString()
-                }
+        val btn_cong: Button = findViewById(R.id.btn_addition)
+        btn_cong.setOnClickListener {
+            if (checkNull(num1, num2)) {
+                result.text = "Hãy nhập đủ 2 số"
+                println("AAAAAAA")
+            }else{
+                val a: Double = num1.text.toString().toDouble()
+                val b: Double = num2.text.toString().toDouble()
+                val res = a + b
+                result.text = res.toString()
             }
         }
+
+        val btn_tru: Button = findViewById(R.id.btn_subtraction)
+        btn_tru.setOnClickListener {
+            if (checkNull(num1, num2)) {
+                result.text = "Hãy nhập đủ 2 số"
+            }else{
+                val a: Double = num1.text.toString().toDouble()
+                val b: Double = num2.text.toString().toDouble()
+                val res = a - b
+                result.text = res.toString()
+            }
+        }
+
+        val btn_nhan: Button = findViewById(R.id.btn_multiply)
+        btn_nhan.setOnClickListener {
+            if (checkNull(num1, num2)) {
+                result.text = "Hãy nhập đủ 2 số"
+            }else{
+                val a: Double = num1.text.toString().toDouble()
+                val b: Double = num2.text.toString().toDouble()
+                val res = a * b
+                result.text = res.toString()
+            }
+        }
+
+        val btn_chia: Button = findViewById(R.id.btn_division)
+        btn_chia.setOnClickListener {
+            if (checkNull(num1, num2)) {
+                result.text = "Hãy nhập đủ 2 số"
+            }else{
+                val a: Double = num1.text.toString().toDouble()
+                val b: Double = num2.text.toString().toDouble()
+                val res = a / b
+                result.text = res.toString()
+            }
+        }
+    }
+
+    fun checkNull (temp1 : EditText, temp2 : EditText) : Boolean {
+        if (temp1.text.toString() == "" || temp2.text.toString() == ""){
+            return true
+        }
+        return false
     }
 }
